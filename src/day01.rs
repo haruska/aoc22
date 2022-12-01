5e5a9f48c5181f6606992ef31d25d8f23e936348
haruska <2803+haruska@users.noreply.github.com>
2022-12-01 17:37:52 -0600

    improvements to day one code after initial solution (#1)
    * use iterators for parse. unwrap_or for error handling
    
    * use itertools and slices
    
    * remove various unwraps

--- src/day01.rs
+++ src/day01.rs
@@ -1,41 +1,30 @@
-use std::error;
+use itertools::{max, sorted};
+use std::error::Error;
 use std::num::ParseIntError;
 
 fn parse(input: &str) -> Result<Vec<usize>, ParseIntError> {
-    let mut elves: Vec<usize> = vec![];
-
-    let mut sum = 0;
-    for line in input.split('\n') {
-        if line.is_empty() {
-            elves.push(sum);
-            sum = 0;
-        } else {
-            let x: usize = line.to_string().parse()?;
-            sum += x;
-        }
-    }
-    if sum != 0 {
-        elves.push(sum);
-    }
-
-    Ok(elves)
+    input
+        .split("\n\n")
+        .map(|s| s.lines().map(|s| s.to_string().parse::<usize>()).sum())
+        .collect()
 }
 
 fn top_elf(elves: &[usize]) -> Option<usize> {
-    elves.iter().max().copied()
+    max(elves).copied()
 }
 
-fn top_elves(mut elves: Vec<usize>, count: usize) -> usize {
-    elves.sort();
-    let top = &elves[elves.len() - count..];
-    top.iter().sum()
+fn top_elves(elves: &[usize], count: usize) -> usize {
+    let skip_len = elves.len() - count;
+    sorted(elves).skip(skip_len).sum()
 }
 
-fn main() -> Result<(), Box<dyn error::Error>> {
+fn main() -> Result<(), Box<dyn Error>> {
     let input = include_str!("../input/day01.txt");
-    let elves = parse(input)?;
 
-    let top_elf = top_elf(elves.as_slice()).unwrap();
+    let elves = parse(input)?;
+    let elves = elves.as_slice();
+
+    let top_elf = top_elf(elves).expect("Could not find a top elf.");
     println!("Top elf (part 1): {}", top_elf);
 
     let top_elves = top_elves(elves, 3);
@@ -51,19 +40,19 @@ mod tests {
     #[test]
     fn parse_elf_sums() {
         let input = include_str!("../input/day01_test.txt");
-        let result = parse(input).unwrap();
-        assert_eq!(result, [6000, 4000, 11000, 24000, 10000]);
+        let result = parse(input);
+        assert_eq!(result, Ok(vec![6000, 4000, 11000, 24000, 10000]));
     }
 
     #[test]
     fn find_max_elf() {
-        let result = top_elf(&[6000, 4000, 11000, 24000, 10000]).unwrap();
-        assert_eq!(result, 24000);
+        let result = top_elf(&[6000, 4000, 11000, 24000, 10000]);
+        assert_eq!(result, Some(24000));
     }
 
     #[test]
     fn find_top_sum() {
-        let result = top_elves(vec![6000, 4000, 11000, 24000, 10000], 3);
+        let result = top_elves(&[6000, 4000, 11000, 24000, 10000], 3);
         assert_eq!(result, 45000);
     }
 }
