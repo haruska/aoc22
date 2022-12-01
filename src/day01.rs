e2670774f62ae0a07175ada59a81daae699d2c78
Jason Haruska <jason.haruska@shopify.com>
2022-12-01 17:31:11 -0600

    remove various unwraps

--- src/day01.rs
+++ src/day01.rs
@@ -1,17 +1,16 @@
 use itertools::{max, sorted};
+use std::error::Error;
+use std::num::ParseIntError;
 
-fn parse(input: &str) -> Vec<usize> {
+fn parse(input: &str) -> Result<Vec<usize>, ParseIntError> {
     input
         .split("\n\n")
-        .map(|s| {
-            s.split('\n')
-                .fold(0, |a, s2| a + s2.to_string().parse().unwrap_or(0))
-        })
+        .map(|s| s.lines().map(|s| s.to_string().parse::<usize>()).sum())
         .collect()
 }
 
-fn top_elf(elves: &[usize]) -> usize {
-    max(elves).copied().unwrap_or(0)
+fn top_elf(elves: &[usize]) -> Option<usize> {
+    max(elves).copied()
 }
 
 fn top_elves(elves: &[usize], count: usize) -> usize {
@@ -19,17 +18,19 @@ fn top_elves(elves: &[usize], count: usize) -> usize {
     sorted(elves).skip(skip_len).sum()
 }
 
-fn main() {
+fn main() -> Result<(), Box<dyn Error>> {
     let input = include_str!("../input/day01.txt");
 
-    let elves = parse(input);
+    let elves = parse(input)?;
     let elves = elves.as_slice();
 
-    let top_elf = top_elf(elves);
+    let top_elf = top_elf(elves).expect("Could not find a top elf.");
     println!("Top elf (part 1): {}", top_elf);
 
     let top_elves = top_elves(elves, 3);
     println!("Sum top elves (part 2): {}", top_elves);
+
+    Ok(())
 }
 
 #[cfg(test)]
@@ -40,13 +41,13 @@ mod tests {
     fn parse_elf_sums() {
         let input = include_str!("../input/day01_test.txt");
         let result = parse(input);
-        assert_eq!(result, [6000, 4000, 11000, 24000, 10000]);
+        assert_eq!(result, Ok(vec![6000, 4000, 11000, 24000, 10000]));
     }
 
     #[test]
     fn find_max_elf() {
         let result = top_elf(&[6000, 4000, 11000, 24000, 10000]);
-        assert_eq!(result, 24000);
+        assert_eq!(result, Some(24000));
     }
 
     #[test]
