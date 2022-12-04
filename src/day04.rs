e63aa3dd659b78e497ee6de1d1ae16e7e0b7c832
haruska <2803+haruska@users.noreply.github.com>
2022-12-04 14:11:57 -0600

    refactor day 4. add tests

--- src/day04.rs
+++ src/day04.rs
@@ -32,25 +32,77 @@ fn overlap(wp: WorkerPair) -> bool {
     (a <= y && a >= x) || (b <= y && b >= x) || (x <= b && x >= a) || (y <= b && y >= a)
 }
 
+fn contained_count(worker_pairs: &[WorkerPair]) -> usize {
+    worker_pairs.iter().fold(
+        0,
+        |acc, wp| {
+            if fully_contained(*wp) {
+                acc + 1
+            } else {
+                acc
+            }
+        },
+    )
+}
+
+fn overlap_count(worker_pairs: &[WorkerPair]) -> usize {
+    worker_pairs
+        .iter()
+        .fold(0, |acc, wp| if overlap(*wp) { acc + 1 } else { acc })
+}
+
 fn main() -> Result<(), Box<dyn Error>> {
     let input = include_str!("../input/day04.txt");
-    let elf_pairs = parse(input);
-    let contained_count: usize =
-        elf_pairs.iter().fold(
-            0,
-            |acc, wp| {
-                if fully_contained(*wp) {
-                    acc + 1
-                } else {
-                    acc
-                }
-            },
-        );
 
     let elf_pairs = parse(input);
-    let overlap_count: usize = elf_pairs
-        .iter()
-        .fold(0, |acc, wp| if overlap(*wp) { acc + 1 } else { acc });
-    println!("{:?}, {:?}", contained_count, overlap_count);
+    let elf_pairs = elf_pairs.as_slice();
+
+    let part_one = contained_count(elf_pairs);
+    println!("Contained count (part 1): {:?}", part_one);
+
+    let part_two = overlap_count(elf_pairs);
+    println!("Overlap Count (part 2): {:?}", part_two);
+
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn worker_pairs() -> Vec<WorkerPair> {
+        vec![
+            ((2, 4), (6, 8)),
+            ((2, 3), (4, 5)),
+            ((5, 7), (7, 9)),
+            ((2, 8), (3, 7)),
+            ((6, 6), (4, 6)),
+            ((2, 6), (4, 8)),
+        ]
+    }
+
+    #[test]
+    fn parse_test() {
+        let input = include_str!("../input/day04_test.txt");
+        let result = parse(input);
+        let expected = worker_pairs();
+
+        assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn contained_count_test() {
+        let wp = worker_pairs();
+        let result = contained_count(wp.as_slice());
+
+        assert_eq!(result, 2);
+    }
+
+    #[test]
+    fn overlap_count_test() {
+        let wp = worker_pairs();
+        let result = overlap_count(wp.as_slice());
+
+        assert_eq!(result, 4);
+    }
 }
