b700284aba3460d6d85ee279cf33672dafec9900
haruska <2803+haruska@users.noreply.github.com>
2022-12-05 10:02:42 -0600

    Implement parse by fixed width and regex

--- src/day05.rs
+++ src/day05.rs
@@ -1,47 +1,49 @@
+use recap::Recap;
+use serde::Deserialize;
 use std::error::Error;
 
 type Stack = Vec<char>;
 
+#[derive(Debug, Deserialize, Recap, PartialEq)]
+#[recap(regex = r#"move (?P<num_crates>\d+) from (?P<from>\d+) to (?P<to>\d+)"#)]
 struct Direction {
-    stack_num: usize,
     num_crates: usize,
     from: usize,
     to: usize,
 }
 
-fn parse(input: &str) -> (Vec<Stack>, Vec<Direction>) {
-    let (stacks_input, _directions_input) = input.split_once("\n\n").unwrap();
-    let stack_vals: Vec<Vec<Option<char>>> = stacks_input
-        .lines()
-        .rev()
-        .skip(1)
-        .map(|line| {
-            let vals = line
-                .as_bytes()
-                .chunks(4)
-                .map(|cr| {
-                    let c = cr[1] as char;
-                    if c == ' ' {
-                        None
-                    } else {
-                        Some(c)
-                    }
-                })
-                .collect();
-            vals
-        })
-        .collect();
+fn parse_stacks(input: &str) -> Vec<Stack> {
+    let mut lines = input.lines().rev();
 
-    let mut stacks: Vec<Stack> = vec![vec![]; stack_vals[0].len()];
-    for sval in stack_vals.iter() {
-        for (i, c) in sval.iter().enumerate() {
-            if let Some(c) = c {
-                stacks[i].push(*c);
+    // the first line is a header with the stack numbers. use it to count the number of stacks.
+    let num_stacks = lines.next().unwrap().as_bytes().chunks(4).count();
+    let mut stacks: Vec<Stack> = vec![vec![]; num_stacks];
+
+    for line in lines {
+        for (i, cr) in line.as_bytes().chunks(4).enumerate() {
+            let c = cr[1] as char;
+            if c != ' ' {
+                stacks[i].push(c);
             }
         }
     }
 
-    (stacks, vec![])
+    stacks
+}
+
+fn parse_directions(input: &str) -> Result<Vec<Direction>, Box<dyn Error>> {
+    let mut res = vec![];
+    for line in input.lines() {
+        let d: Direction = line.parse()?;
+        res.push(d);
+    }
+    Ok(res)
+}
+
+fn parse(input: &str) -> (Vec<Stack>, Vec<Direction>) {
+    let (stacks, directions) = input.split_once("\n\n").unwrap();
+
+    (parse_stacks(stacks), parse_directions(directions).unwrap())
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
@@ -56,11 +58,37 @@ mod tests {
         vec![vec!['Z', 'N'], vec!['M', 'C', 'D'], vec!['P']]
     }
 
+    fn directions_fixture() -> Vec<Direction> {
+        vec![
+            Direction {
+                num_crates: 1,
+                from: 2,
+                to: 1,
+            },
+            Direction {
+                num_crates: 3,
+                from: 1,
+                to: 3,
+            },
+            Direction {
+                num_crates: 2,
+                from: 2,
+                to: 1,
+            },
+            Direction {
+                num_crates: 1,
+                from: 1,
+                to: 2,
+            },
+        ]
+    }
+
     #[test]
     fn parse_test() {
         let input = include_str!("../input/day05_test.txt");
-        let (stacks, _directions) = parse(input);
+        let (stacks, directions) = parse(input);
 
         assert_eq!(stacks, stacks_fixture());
+        assert_eq!(directions, directions_fixture());
     }
 }
